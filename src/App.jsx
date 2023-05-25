f8f4912a2597443004ece1ca3c8a3e0258d91a3a
mayanks11 <mayanks03@outlook.com>
2023-05-25 15:13:33 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import{ BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Components/Navbar";
 export default function App(){
     return(
-        <div>
-            <p>Hello</p>
+        <div className="app">
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path = "/"/>
+                    <Route path = "/cart"/>
+                </Routes>
+            </Router>
         </div>
     )
 }
