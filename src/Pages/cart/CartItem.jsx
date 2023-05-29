4f98fb70a7cd3c737e70ad593f410a79d62787e0
mayanks11 <mayanks03@outlook.com>
2023-05-29 16:36:24 +0530

    add and remove from cart

--- src/Pages/cart/CartItem.jsx
+++ src/Pages/cart/CartItem.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./cart.css";
+import { ShopContext } from "../../context/shop-context";
 
 export default function CartItem(props){
     const{id, productName, price, productImage} = props.data;
+    const {cartItems, addToCart, removeFromCart} = useContext(ShopContext);
     return(
         <div className="cartItem">
             <img src = {productImage}/>
             <div className="description">
                 <p><b>{productName}</b></p>
                 <p><b>${price}</b></p>
+                <div className="countHandler">
+                    <button onClick={() => removeFromCart(id)}>-</button>
+                    <input value={cartItems[id]}/>
+                    <button onClick={() => addToCart(id)}>+</button>
+                </div>
             </div>
         </div>
     )
